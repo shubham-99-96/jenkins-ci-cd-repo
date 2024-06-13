28d1167884e357ffbae54b31d63f3df42cc1483a
Shubham D. Naglot <73437656+shubham-99-96@users.noreply.github.com>
2024-06-13 14:41:28 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,9 @@
-ping google.com
-ipconfig
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php echo '<p>Hello World</p>'; ?>
+    </body>
+</html>
