0ffa4d05288a839c2b80c138b918c96dc58d421d
Shubham D. Naglot <73437656+shubham-99-96@users.noreply.github.com>
2024-06-21 12:37:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
         <title>PHP Test</title>
     </head>
     <body>
-        <?php echo '<p>Hello World test pipeline 1</p>'; ?>
+        <?php echo '<p>Hello World test pipeline 23</p>'; ?>
     </body>
 </html>
